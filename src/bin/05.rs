a814584289e143ece93e2d50cba22d6cb35e6de9
Nick Kress <kressnick25@gmail.com>
2023-12-05 07:22:46 +0000

    Day5 ?

---
+++ src/bin/05.rs
@@ -0,0 +1,155 @@
+use advent_of_code::find_numbers;
+use regex::Regex;
+use std::str::FromStr;
+
+advent_of_code::solution!(5);
+
+#[derive(Debug, PartialEq)]
+enum MapType {
+    SeedToSoil,
+    SoilToFert,
+    FertToWater,
+    WaterToLight,
+    LightToTemp,
+    TempToHumid,
+    HumidToLoc
+}
+
+impl FromStr for MapType {
+    type Err = String;
+
+    fn from_str(input: &str) -> Result<MapType, Self::Err> {
+        match input {
+            "seed-to-soil" => Ok(MapType::SeedToSoil),
+            "soil-to-fertilizer" => Ok(MapType::SoilToFert),
+            "fertilizer-to-water" => Ok(MapType::FertToWater),
+            "water-to-light" => Ok(MapType::WaterToLight),
+            "light-to-temperature" => Ok(MapType::LightToTemp),
+            "temperature-to-humidity" => Ok(MapType::TempToHumid),
+            "humidity-to-location" => Ok(MapType::HumidToLoc),
+            _ => Err(input.to_string())
+        }
+    }
+}
+
+#[derive(Debug)]
+struct MappingRange {
+    destination_start: u32,
+    source_start: u32,
+    length: u32
+}
+
+impl MappingRange {
+    fn source_end(&self) -> u32 {
+       self.source_start + self.length
+    }
+
+    fn destination_end(&self) -> u32 {
+        self.destination_start + self.length
+    }
+
+    fn can_translate(&self, source: u32) -> bool {
+        return source >= self.source_start && source <= self.source_end();
+    }
+
+    fn translate(&self, source: u32) -> u32 {
+        return self.destination_start + source.abs_diff(self.source_start);
+    }
+}
+
+#[derive(Debug)]
+struct Mapping {
+    mtype: MapType,
+    ranges: Vec<MappingRange>
+}
+
+impl Mapping {
+    fn default() -> Mapping {
+        Mapping{mtype: MapType::SeedToSoil, ranges: vec![]}
+    }
+}
+
+fn seed_to_location(mappings: &Vec<Mapping>, seed: &u32) -> u32 {
+    let mut next = seed.clone();
+    print!("\n");
+    for mapping in mappings {
+        for range in &mapping.ranges {
+            if range.can_translate(next) {
+                let temp = next.clone();
+                next = range.translate(next);
+                println!("translated {}, to {}, for stage {:?}", temp, next, mapping.mtype);
+                break;
+            }
+            else {
+                println!("stage {:?}", mapping.mtype)
+            }
+        }
+    }
+
+    next
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let map_type_re = Regex::new(r"^(?<name>[a-z-]+)").unwrap();
+
+    let mut input = input.lines();
+
+    let _seed_line = input.next().unwrap();
+    let seeds = find_numbers(_seed_line);
+
+    let mut mappings: Vec<Mapping> = vec![];
+    let mut current_mapping = Mapping::default();
+
+    let input = input.skip(1);
+    let max_index = input.clone().count() - 1;
+
+    for (i, line) in input.enumerate() {
+        if line.chars().nth(0).is_some_and(|c| c.is_alphabetic()) {
+            let caps = map_type_re.captures(line).unwrap();
+            current_mapping.mtype = MapType::from_str(&caps["name"]).unwrap();
+            continue;
+        }
+        if line.chars().nth(0).is_some_and(|c| c.is_numeric()) {
+            let nums = find_numbers(line);
+            let range = MappingRange{
+                destination_start: nums[0],
+                source_start: nums[1],
+                length: nums[2]
+            };
+            current_mapping.ranges.push(range);
+        }
+
+        if line.trim().is_empty() || i == max_index {
+            mappings.push(current_mapping);
+            current_mapping = Mapping::default();
+        }
+    }
+
+    let locations = seeds
+    .iter()
+    .map(|s| seed_to_location(&mappings, s));
+
+
+    Some(locations.min().unwrap())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(35))
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
